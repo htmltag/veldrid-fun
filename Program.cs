276343d61d1c9d917e11973773523ba99808bc2f
jonathan <jonathan@wiv.io>
2020-03-29 12:17:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+namespace Veldrid4Fun
+{
+    class Program
+    {
+
+        private static GraphicsDevice _graphicsDevice;
+        static void Main(string[] args)
+        {
+            WindowCreateInfo windowCI = new WindowCreateInfo()
+            {
+                X = 100,
+                Y = 100,
+                WindowWidth = 960,
+                WindowHeight = 540,
+                WindowTitle = "Veldrid Tutorial"
+            };
+            Sdl2Window window = VeldridStartup.CreateWindow(ref windowCI);
+
+            _graphicsDevice = VeldridStartup.CreateGraphicsDevice(window);
+
+            while (window.Exists)
+            {
+                window.PumpEvents();
+            }
+
+        }
+    }
+}
